ecd72f610807a4a9e810c5c6ec5eb0d1f57f0622
FORMAL-PC\key <nietononosyana.tarutaru@gmail.com>
2018-10-16 18:53:16 +0900

    unitychan test

---
+++ Unitychan_test/Assets/Player.cs
@@ -0,0 +1,44 @@
+﻿// Player.cs
+using UnityEngine;
+
+// プレイヤー
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 velocity;              // 移動方向
+    [SerializeField] private float moveSpeed = 5.0f;        // 移動速度
+    [SerializeField] private float applySpeed = 0.2f;       // 振り向きの適用速度
+    [SerializeField] private PlayerFollowCamera refCamera;  // カメラの水平回転を参照する用
+
+    void Update()
+    {
+        // WASD入力から、XZ平面(水平な地面)を移動する方向(velocity)を得ます
+        velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            velocity.z += 1;
+        if (Input.GetKey(KeyCode.A))
+            velocity.x -= 1;
+        if (Input.GetKey(KeyCode.S))
+            velocity.z -= 1;
+        if (Input.GetKey(KeyCode.D))
+            velocity.x += 1;
+
+        // 速度ベクトルの長さを1秒でmoveSpeedだけ進むように調整します
+        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
+
+        // いずれかの方向に移動している場合
+        if (velocity.magnitude > 0)
+        {
+            // プレイヤーの回転(transform.rotation)の更新
+            // 無回転状態のプレイヤーのZ+方向(後頭部)を、
+            // カメラの水平回転(refCamera.hRotation)で回した移動の反対方向(-velocity)に回す回転に段々近づけます
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(refCamera.hRotation * velocity),
+                                                  applySpeed);
+
+            // プレイヤーの位置(transform.position)の更新
+            // カメラの水平回転(refCamera.hRotation)で回した移動方向(velocity)を足し込みます
+            transform.position += refCamera.hRotation * velocity;
+        }
+    }
+}
